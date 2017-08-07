a135b044ba1d8b0e63d2bd966895f80303084fe5
ljmlance <ljm2423124756@icloud.com>
2017-08-08 05:20:23 +0800

    项目初始化

---
+++ allkindsImages/allkindsImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  allkindsImages
+//
+//  Created by ljm on 2017/8/8.
+//  Copyright © 2017年 ljm.ynyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ allkindsImages/allkindsImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  allkindsImages
+//
+//  Created by ljm on 2017/8/8.
+//  Copyright © 2017年 ljm.ynyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
